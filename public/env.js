5204150f72bf4ff4fb6f131b93da436354abee4a
GuiPoZhou <1207336828@qq.com>
2024-08-29 16:40:49 +0800

    init project

---
+++ public/env.js
@@ -0,0 +1,54 @@
+/*
+* @Author: 焦政
+* @Date: 2021-07-02 08:43:02
+* @LastEditTime: 2021-07-02 10:59:49
+* @Description:
+*/
+window.globalEnv = {
+    "LOAD_MICRO_LIST": ['micGenric', 'micFoundation', 'micProject', 'micEln', 'micIot', 'micEnvExtension'],
+    // 'VUE_APP_BASE_API': `http://192.168.36.100:8082`, // 华淼
+    // 'VUE_APP_BASE_API': `http://192.168.36.100:8086`, // 大咖
+    // 'VUE_APP_BASE_API': `http://192.168.36.100:8089`, // 大咖
+    'VUE_APP_BASE_API': `http://192.168.36.103:8088`, // 大咖
+    // 'VUE_APP_BASE_API': `http://192.168.36.103:8092`, //
+    // 'VUE_APP_BASE_API': `http://192.168.36.162:8086`,
+    // 'VUE_APP_BASE_API': `http://192.168.36.162:8087`,
+    // 'VUE_APP_BASE_API': `http://192.168.36.162:8082`,
+    // 'VUE_APP_BASE_API': `http://192.168.36.163:8087`,
+    // 'VUE_APP_BASE_API': `http://192.168.36.175:9081`, // 泰富 杨建文
+    // 'VUE_APP_BASE_API': `http://192.168.36.175:9083`, // 农大 杨建文
+    // 'VUE_APP_BASE_API': `http://192.168.36.193:9087`, // 王学超 格林美
+    // 'VUE_APP_BASE_API': `http://192.168.36.125:8086`, // 大咖
+    // 'VUE_APP_BASE_API': `http://192.168.36.125:8082`,
+    // 'VUE_APP_BASE_API': `http://192.168.36.215:8089`, // 赵方刚
+    // 'VUE_APP_BASE_API': `http://192.168.36.58:6970/prod-api/food`, // 食农 食药院 食品
+    // 'VUE_APP_BASE_API': `http://192.168.36.58:9095`, // 食农 药品
+    // 'VUE_APP_BASE_API': `http://192.168.36.58:5085/prod-api`, // 克拉玛依
+    // 'VUE_APP_BASE_API': `http://192.168.36.51:5084/prod-api`, // 基线版
+    // 'VUE_APP_BASE_API': `http://192.168.36.53:5090/prod-api`, // 鼎信
+    // 'VUE_APP_BASE_API': `http://192.168.36.53:5082/prod-api`, // 菲尼克斯
+    // 'VUE_APP_BASE_API': `http://192.168.36.53:5087/prod-api`, // 格林美
+    // 'VUE_APP_BASE_API': `http://192.168.36.53:6060/prod-api`, // 电工
+    // 'VUE_APP_BASE_API': `http://192.168.36.53:5083/prod-api`, // 纳通
+    // 'VUE_APP_BASE_API': `http://192.168.36.53:5084/prod-api`, // 华淼
+    // 'VUE_APP_BASE_API': `http://192.168.36.52:9090/prod-api`, // 泰富
+    // 'VUE_APP_BASE_API': `http://192.168.36.52:5062/prod-api`, // 史丹利
+    // 'VUE_APP_BASE_API': `http://192.168.36.53:5088/prod-api`, // 泰德
+    // 'VUE_APP_BASE_API': `http://192.168.36.59:5084/prod-api`, // 东方钽业
+    // 'VUE_APP_BASE_API': `http://192.168.36.60:6110/prod-api`, // 盘锦公安
+    // 'VUE_APP_BASE_API': `http://192.168.36.60:6060/prod-api`, // 青海南玻
+    // 'VUE_APP_BASE_API': `http://192.168.36.60:5062/prod-api`, // 信义硅业
+    // 'VUE_APP_BASE_API': `http://192.168.36.69:5001/prod-api`, // 大咖国际
+    // 'VUE_APP_BASE_API': `http://192.168.36.69:6090/prod-api`, // 山东农大
+    // 'VUE_APP_BASE_API': `http://192.168.36.162:8089`, // 信义硅业
+    // 'VUE_APP_BASE_API': `http://192.168.36.175:9083`, // 青海南玻 杨建文
+    // 'VUE_APP_BASE_API': `http://192.168.36.175:8181`, // 东方钽业 杨建文
+    // 'VUE_APP_BASE_API': `http://192.168.36.221:9095`, // 东方钽业 杨建文
+    // 'VUE_APP_BASE_API': `http://192.168.36.58:6971/prod-api`, // 药品
+    // 'VUE_APP_BASE_API': `http://192.168.36.58:6970/prod-api`, // 药品
+    // 'VUE_APP_BASE_API': `http://192.168.36.58:6972/prod-api`, // 药品
+    // 'VUE_APP_BASE_API': `http://192.168.36.58:9095`, // 动物
+    // 'VUE_APP_BASE_API': `${window.location.protocol}//192.168.36.53:5084/prod-api`, // 华淼
+    'VUE_APP_ELN_SERVER_API': `${window.location.protocol}//192.168.36.53:5084/eln-api`,
+    "VUE_APP_IOT_SERVER_API": `${window.location.protocol}//${window.location.host}/iot-api`,
+}
